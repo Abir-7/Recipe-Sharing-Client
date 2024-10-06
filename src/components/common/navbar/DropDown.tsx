09381f350a44b358fc5f8d91211536552dd2123a
Md. Tazwarul Islam Abir <md.tazwarul.islam.07@gmail.com>
2024-10-06 22:05:20 +0600

    add icon

--- src/components/common/navbar/DropDown.tsx
+++ src/components/common/navbar/DropDown.tsx
@@ -21,7 +21,20 @@ export const DropDown = ({ navLink, user, authLink }: IProps) => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className=" bg-black text-white ">
-        Open
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+          />
+        </svg>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="mt-4 md:hidden">
         {/* dynamic */}
